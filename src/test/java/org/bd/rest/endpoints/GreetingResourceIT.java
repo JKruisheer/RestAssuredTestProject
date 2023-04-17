d56a365e8fbb4fe300d3997703ecd77dabe206ff
Jesse Kruisheer <70973567+JKruisheer@users.noreply.github.com>
2023-04-17 08:44:21 +0200

    main
    Basic quarkus starter

---
+++ src/test/java/org/bd/rest/endpoints/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.bd.rest.endpoints;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
